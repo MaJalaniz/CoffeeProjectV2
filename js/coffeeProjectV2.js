0a3ca2f126c47a02b2d583a0f229e2eed3dd3f40
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-11-26 19:26:15 -0600

    Printed coffees to the brower!

--- js/coffeeProjectV2.js
+++ js/coffeeProjectV2.js
@@ -2,21 +2,82 @@
 //----------------------------------------------------------------------------------------------
         //Coffee names
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {
+        id: 1, 
+        name: 'Light City', 
+        roast: 'light'
+    },
+    {
+        id: 2, 
+        name: 'Half City', 
+        roast: 'light'
+    },
+    {
+        id: 3,
+        name: 'Cinnamon',
+        roast: 'light'
+        },
+    {
+        id: 4,
+        name: 'City',
+        roast: 'medium'
+    },
+    {
+        id: 5,
+        name: 'American',
+        roast: 'medium'
+    },
+    {
+        id: 6,
+        name: 'Breakfast', 
+        roast: 'medium'
+    },
+    {
+        id: 7,
+         name: 'High',
+          roast: 'dark'
+    },
+    {
+        id: 8,
+         name: 'Continental',
+          roast: 'dark'
+    },
+    {
+        id: 9,
+        name: 'New Orleans',
+        roast: 'dark'
+    },
+    {
+        id: 10,
+        name: 'European',
+        roast: 'dark'
+    },
+    {
+        id: 11,
+         name: 'Espresso',
+          roast: 'dark'
+    },
+    {
+        id: 12,
+         name: 'Viennese',
+          roast: 'dark'
+    },
+    {
+        id: 13,
+        name: 'Italian',
+        roast: 'dark'
+    },
+    {
+        id: 14,
+         name: 'French',
+          roast: 'dark'
+    },
 ];
+
+//Displays the coffees to the browser
+for (var i=0; i < coffees.length ;i++)
+    document.getElementById("myDiv").innerHTML += "Name: " + coffees[i].name + " Roast: "+ coffees[i].roast + "<br>";
+
 
 //----------------------------------------------------------------------------------------------
 
@@ -24,7 +85,7 @@ let coffees = [
 
 function searching(){
     let bar = document.getElementById('searchBar').value;
-    let grab = document.querySelector('.box');
+    //let grab = document.querySelector('.box');
     //grab.textContent = bar; //prints search to browser
     
     for(let i = 0; i < coffees.length; i++){
@@ -41,18 +102,27 @@ function searching(){
     //console.log(bar);
     }
 }
+
+//----------------------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------------------------
 //this function logs the coffee and the individual letter
+/*
 function little(){
     for(let j = 0; j < coffees.length; j++){
         for(let k = 0; k < coffees.length; k++){
-        console.log(coffees[j].name[k]);
+        //console.log(coffees[j].name[k]);
+        let roasts = coffees[j].name[k];
+        console.log(roasts);
+        //let letters = document.getElementById('box');
+       // letters.textContent = roasts;
         }
     }
 }
 console.log(little());
-
+*/
 //Bold the name
 //Include the roast in smaller font
 
 //----------------------------------------------------------------------------------------------
-
