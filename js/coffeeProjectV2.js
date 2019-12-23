c381a31b1a26752cf17307575ad1a152c2213e6a
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-12-22 20:59:54 -0600

    fixed a JS error that borked coffee arrangement

--- js/coffeeProjectV2.js
+++ js/coffeeProjectV2.js
@@ -1,78 +1,39 @@
 
 //----------------------------------------------------------------------------------------------
         //Coffee names
-let coffees = [
-    {
-        id: 1, 
-        name: 'Light City', 
-        roast: 'light'
-    },
-    {
-        id: 2, 
-        name: 'Half City', 
-        roast: 'light'
-    },
-    {
-        id: 3,
-        name: 'Cinnamon',
-        roast: 'light'
-        },
-    {
-        id: 4,
-        name: 'City',
-        roast: 'medium'
-    },
-    {
-        id: 5,
-        name: 'American',
-        roast: 'medium'
-    },
-    {
-        id: 6,
-        name: 'Breakfast', 
-        roast: 'medium'
-    },
-    {
-        id: 7,
-         name: 'High',
-          roast: 'dark'
-    },
-    {
-        id: 8,
-         name: 'Continental',
-          roast: 'dark'
-    },
-    {
-        id: 9,
-        name: 'New Orleans',
-        roast: 'dark'
-    },
-    {
-        id: 10,
-        name: 'European',
-        roast: 'dark'
-    },
-    {
-        id: 11,
-         name: 'Espresso',
-          roast: 'dark'
-    },
-    {
-        id: 12,
-         name: 'Viennese',
-          roast: 'dark'
-    },
-    {
-        id: 13,
-        name: 'Italian',
-        roast: 'dark'
-    },
-    {
-        id: 14,
-         name: 'French',
-          roast: 'dark'
-    },
+let coffees = 
+   [{id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light' },
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental' , roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
 ];
+
+function entire(){
+    for (var i=0; i < coffees.length ;i++)
+         document.getElementById("myDiv").innerHTML +=  "<div class= example>" + "<strong>" + coffees[i].name + "</strong>" + " " + "<i>" + coffees[i].roast + "</i>" + "</div>" ;
+         //console.log(give());
+         //MANIPULATE THIS FUNCTION INSTEAD OF THE GIVE() TO DISPLAY COFFEES
+    }
+
+    function darker(){
+        let one = document.getElementById('thisOne').style.backgroundColor = "black";
+        let two = document.getElementById('thisTwo').style.color = "white";
+        let three = document.getElementById('darkMode').innerHTML = "Light Mode";
+        //Write an If statement to toggle between light and dark mode
+        //console.log(three);
+        }
+
+
 
 //Displays the coffees to the browser
 /*
@@ -86,7 +47,7 @@ for (var i=0; i < coffees.length ;i++)
 //----------------------------------------------------------------------------------------------
 
     // Dynamic search function v1.0, This works with New Orleans, but not Light or Half because of the name City.
-
+/*
 function searching(){
     let bar = document.getElementById('searchBar').value;
     //let grab = document.querySelector('.box');
@@ -105,10 +66,11 @@ function searching(){
     
     }
 }
-
+*/
 //----------------------------------------------------------------------------------------------
 
     //Dynamic search function 2.0, now works with all the coffees now not just light or half.
+    /*
 function search_drink() { 
     let input = document.getElementById('searchBar').value 
     input=input.toLowerCase(); 
@@ -123,10 +85,10 @@ function search_drink() {
         } 
     } 
 } 
-
+*/
 //----------------------------------------------------------------------------------------------
     //write a function that adds an item to the list of coffee objects
-
+/*
     function newOption(){
         for (var i=0; i < coffees.length ;i++){
             let those = coffees[i].id;
@@ -150,18 +112,16 @@ function search_drink() {
         console.log();
     }
 console.log(newOption());
-    
+    */
 //----------------------------------------------------------------------------------------------
         //write a function that shows all the light coffees
     //this function does not get rid of the previous coffees
     // instead, adds each category of roasts/coffees at the bottom of the previouse list of coffees
     //All of the options now display their respective choice except for "choose your coffee" which is void
-    for(let b = 0; b < coffees.length; b++){        
-        document.getElementById('myDiv').style.display = 'none';
-}
+   
 
 
-
+/*
     function give() { 
         var x = document.getElementById("choices").value; 
         document.getElementById("choiceHere").innerHTML =  x;
@@ -196,12 +156,5 @@ console.log(newOption());
             }
     
         console.log(x);
-    
+    */
 //---------------------------------------------------------------------------------------------- 
-
-function entire(){
-    for (var i=0; i < coffees.length ;i++)
-         document.getElementById("myDiv").innerHTML +=  "<div class= example>" + "<strong>" + coffees[i].name + "</strong>" + " " + "<i>" + coffees[i].roast + "</i>" + "</div>" + "<br>";
-         console.log(give());
-         //MANIPULATE THIS FUNCTION INSTEAD OF THE GIVE() TO DISPLAY COFFEES
-    }
